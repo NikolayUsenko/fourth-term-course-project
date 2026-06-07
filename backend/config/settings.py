29417478a30e997eea807fb2f52c0789ad2f333e
Николай <usenkko.nikolay@gmail.com>
2026-06-07 17:48:38 +0300

    chore(config): add PostgreSQL database configuration with environment variables

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -88,6 +88,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.getenv('DB_NAME', 'keytype'),
+    #     'USER': os.getenv('DB_USER', 'postgres'),
+    #     'PASSWORD': os.getenv('DB_PASSWORD', ''),
+    #     'HOST': os.getenv('DB_HOST', 'localhost'),
+    #     'PORT': os.getenv('DB_PORT', '5432'),
+    # }
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
